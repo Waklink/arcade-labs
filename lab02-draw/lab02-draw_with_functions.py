d0a2ff8ef24b068d6fd11ff06eab4794947f2b24
Waklink <catalin.dinu@edu.uah.es>
2023-02-16 20:27:51 +0100

    oleaje y nubes añadidas

--- lab02-draw/lab02-draw_with_functions.py
+++ lab02-draw/lab02-draw_with_functions.py
@@ -35,17 +35,51 @@ def barco_con_vela(centro_barco_x, centro_barco_y):
                                 centro_barco_x+x2/2, 2*centro_barco_y, arcade.color.WHITE)
 
 
+def oleaje(window_width, window_height):
+    for i in range(0, window_height, 20):
+        for j in range(0, window_width, 20):
+            arcade.draw_arc_outline(j, i, 20, 20, arcade.color.SKY_BLUE, 0, 90, 1, 0, 256)
+            arcade.draw_arc_outline(j, i, 20, 20, arcade.color.SKY_BLUE, 90, 180, 1, 0, 256)
+            arcade.draw_arc_outline(j, i, 20, 20, arcade.color.SKY_BLUE, 45, 135, 1, 0, 256)
+
+
+def nube(centro_nube, anchura_nube, altura_nube, radio_circulos):
+    arcade.draw_lrtb_rectangle_filled(centro_nube[0]-anchura_nube/2+radio_circulos, centro_nube[0]+anchura_nube/2-radio_circulos,
+                                      centro_nube[1]+altura_nube/2-radio_circulos, centro_nube[1]-altura_nube/2+radio_circulos,
+                                      arcade.color.DARK_GRAY)
+    for i in range(0, anchura_nube-radio_circulos, radio_circulos):
+        arcade.draw_arc_filled(centro_nube[0] - anchura_nube/2 + radio_circulos + i,
+                               centro_nube[1] + altura_nube/2 - radio_circulos,
+                               radio_circulos, radio_circulos, arcade.color.DARK_GRAY, 0, 180, 0, 256)
+    for j in range(0, anchura_nube-radio_circulos, radio_circulos):
+        arcade.draw_arc_filled(centro_nube[0] - anchura_nube / 2 + radio_circulos + j,
+                               centro_nube[1] - altura_nube / 2 + radio_circulos,
+                               radio_circulos, radio_circulos, arcade.color.DARK_GRAY, 180, 360, 0, 256)
+    for k in range(0, altura_nube-radio_circulos, radio_circulos):
+        arcade.draw_arc_filled(centro_nube[0] - anchura_nube / 2 + radio_circulos,
+                               centro_nube[1] - altura_nube / 2 + radio_circulos + k,
+                               radio_circulos, radio_circulos, arcade.color.DARK_GRAY, 90, 270, 0, 256)
+    for r in range(0, altura_nube-radio_circulos, radio_circulos):
+        arcade.draw_arc_filled(centro_nube[0] + anchura_nube / 2 - radio_circulos,
+                               centro_nube[1] - altura_nube / 2 + radio_circulos + r,
+                               radio_circulos, radio_circulos, arcade.color.DARK_GRAY, 270, 450, 0, 256)
+
+
 arcade.open_window(600, 600, 'Drawing Example')
 
 arcade.start_render()
 
 fondo_oceano(600, 600)
-barco_con_vela(210, 65)
+nube((400, 550), 100, 50, 10)
+nube((150, 520), 80, 40, 10)
+nube((60, 540), 80, 40, 8)
 pajaro(50, 60, 530)
 pajaro(65, 76, 535)
 pajaro(55, 79, 550)
 pajaro(70, 100, 515)
 pajaro(40, 68, 505)
+oleaje(600, 500)
+barco_con_vela(210, 65)
 
 arcade.finish_render()
 
